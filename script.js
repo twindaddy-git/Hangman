50113c26c1c2a277af244e683f5b49702c4b3a9f
Markus Steffl <msteffl@gmail.com>
2021-03-15 08:39:03 +0000

    JSDoc - Dokumentation

--- script.js
+++ script.js
@@ -4,9 +4,14 @@
 const MASK_CHARACTER = "_";
 
 /**
- * Eine Klasse zur Darstellung einer Tastatur
+ * Eine Klasse zur Darstellung einer On-Screen-Tastatur
  */
 class Keyboard {
+
+    /**
+     * Erstellt eine On-Screen-Tastatur
+     * @param {string} elementID - Die ID des zugeordneten DOM-Elements
+     */
     constructor(elementID) {
         this._rows = [
             "QWERTZUIOP",
@@ -16,10 +21,18 @@ class Keyboard {
         this._elementID = elementID;
     }
 
+    /**
+     * Das zugeordnete DOM-Element.
+     * @type {object} 
+     */
     get element() {
         return document.getElementById(this._elementID);
     }
 
+    /**
+     * Erstellt eine HTML-Repräsentation einer kleinen Tastatur
+     * @returns {string} Ein Div-Tag mit der Visualisierung der Tastatur
+     */
     renderHtml() {
         var result = '<div class="keyboard"> ';
         for (var i = 0; i < this._rows.length; i++) {
@@ -33,6 +46,10 @@ class Keyboard {
         return result;
     }
 
+    /**
+     * Erssetzt den innertHTML-Bereich des zugeordneten DOM-Elements mit
+     * der HTML-Repräsentation der Tastatur
+     */
     render() {
         this.element.innerHTML = this.renderHtml();
     }
@@ -47,12 +64,20 @@ class Keyboard {
  */
 class HangmanText {
 
+    /**
+     * Erstellt einen HangmanText
+     * @param {string} elementID - Die ID des zugeordneten DOM-Elements
+     */
     constructor(elementID = false) {
         this._text = "";
         this._guessed = "";
         this.connect(elementID);
     }
 
+    /**
+     * Der zu erratende Text
+     * @type {string}
+     */
     get text() {
         return this._text;
     }
@@ -62,18 +87,34 @@ class HangmanText {
         this.reset();
     }
 
-    get element() {
-        return document.getElementById(this._elementID);
-    }
-
+    /**
+     * Der bisher erratene Text. Noch nicht erratene Zeichen sind durch
+     * das MASK_CHARACTER ersetzt.
+     * @type {string}
+     */
     get guessed() {
         return this._guessed;
     }
 
+    /**
+     * Das zugeordnete DOM-Element.
+     * @type {object} 
+     */
+    get element() {
+        return document.getElementById(this._elementID);
+    }
+
+    /**
+     * Verbindet den HangmanText mit einem DOM-Element
+     * @param {string} elementID - Die ID des zugeordneten DOM-Elements
+     */
     connect(elementID) {
         this._elementID = elementID;
     }
 
+    /**
+     * Setzt den Ratetext und die Anzahl der (Fehl)Versuche auf den Ausgangszustand zurück.
+     */
     reset() {
         this._tries = 0;
         this._fails = 0;
@@ -89,21 +130,38 @@ class HangmanText {
         }
     }
 
+    /**
+     * Der Lösungzustand des Ratetexts
+     * @returns {boolean} Liefert true, wenn das Rätsel gelöst ist.
+     */
     isSolved() {
         return this._guessed.indexOf(MASK_CHARACTER) < 0;
     }
 
+    /**
+     * Verbindet den Ratetext mit einen On-Screen-Keyboard, das die 
+     * Eingaben für die geratenen Buchstaben liefert
+     * @param {Keyboard} keyboard - Die Instanz einer On-Screen-Tastatur
+     */
     registerKeyboard(keyboard) {
         this._keyboard = keyboard;
         keyboard.element.addEventListener("click", ev => this.onClick(ev));
     }
 
+    /**
+     * EventListener für Klicks auf die zugeordnete On-Screen-Tastatur
+     * @param {Event} ev - Das auslösende Click-Event
+     */
     onClick(ev) {
         if (ev.target.id) {
             this.guess(ev.target.id);
         }
     }
 
+    /**
+     * Prüft einen Rateversuch. Der Vergleich ignoriert Groß-/Kleinschreibung
+     * @param {string} char - Das zu prüfende Zeichen
+     */
     guess(char) {
         var check = char.toUpperCase();
         var found = false;
@@ -111,17 +169,17 @@ class HangmanText {
         for (var i = 0; i < this._text.length; i++) {
             if (this._text[i].toUpperCase() == check) {
                 this._guessed = this._guessed.substring(0, i) + this._text[i] +
-                                this._guessed.substring(i + 1);
+                    this._guessed.substring(i + 1);
                 found = true;
             }
         }
-        if (! found) {
+        if (!found) {
             this._fails++;
             if (this._keyboard) {
                 this._keyboard.setKeyColor(check, "#FFC0C0");
             }
         }
-        if (this._elementID) {            
+        if (this._elementID) {
             this.element.innerText = this._guessed;
         }
         if (this.isSolved()) {
